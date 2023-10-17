7845eab0998530ba9982b1cffb63269400fcbd0d
Nycollas Sobolevski <112396150+NycollasSobolevski@users.noreply.github.com>
2023-10-17 17:29:16 -0300

    Update UserListCard.js

--- GamaTeam/Components/UserListCard.js
+++ GamaTeam/Components/UserListCard.js
@@ -1,9 +1,15 @@
-export default function UserListCard () {
+import { useEffect, useState } from "react";
 
+export default function UserListCard () {
+    const [userList, setUserlist] = useState([]);
+
+    useEffect(() => {
+        setUserlist(...userList, sessionStorage.getItem('userlist'));
+    })
 
     return (
         <>
-            
+
         </>
     )
 }
